85cfb251c81716d1f866a9a84e9b069d795209b2
Vincent HAYS <vincenthays95@gmail.com>
2024-09-19 16:03:16 +0200

    rename extension

---
+++ src/bigtable2_extension.cpp
@@ -0,0 +1,78 @@
+#define DUCKDB_EXTENSION_MAIN
+
+#include "bigtable2_extension.hpp"
+#include "duckdb.hpp"
+#include "duckdb/common/exception.hpp"
+#include "duckdb/common/string_util.hpp"
+#include "duckdb/function/scalar_function.hpp"
+#include "duckdb/main/extension_util.hpp"
+#include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
+
+// OpenSSL linked through vcpkg
+#include <openssl/opensslv.h>
+
+namespace duckdb {
+
+inline void Bigtable2ScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
+    auto &name_vector = args.data[0];
+    UnaryExecutor::Execute<string_t, string_t>(
+	    name_vector, result, args.size(),
+	    [&](string_t name) {
+			return StringVector::AddString(result, "Bigtable2 "+name.GetString()+" 🐥");;
+        });
+}
+
+inline void Bigtable2OpenSSLVersionScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
+    auto &name_vector = args.data[0];
+    UnaryExecutor::Execute<string_t, string_t>(
+	    name_vector, result, args.size(),
+	    [&](string_t name) {
+			return StringVector::AddString(result, "Bigtable2 " + name.GetString() +
+                                                     ", my linked OpenSSL version is " +
+                                                     OPENSSL_VERSION_TEXT );;
+        });
+}
+
+static void LoadInternal(DatabaseInstance &instance) {
+    // Register a scalar function
+    auto bigtable2_scalar_function = ScalarFunction("bigtable2", {LogicalType::VARCHAR}, LogicalType::VARCHAR, Bigtable2ScalarFun);
+    ExtensionUtil::RegisterFunction(instance, bigtable2_scalar_function);
+
+    // Register another scalar function
+    auto bigtable2_openssl_version_scalar_function = ScalarFunction("bigtable2_openssl_version", {LogicalType::VARCHAR},
+                                                LogicalType::VARCHAR, Bigtable2OpenSSLVersionScalarFun);
+    ExtensionUtil::RegisterFunction(instance, bigtable2_openssl_version_scalar_function);
+}
+
+void Bigtable2Extension::Load(DuckDB &db) {
+	LoadInternal(*db.instance);
+}
+std::string Bigtable2Extension::Name() {
+	return "bigtable2";
+}
+
+std::string Bigtable2Extension::Version() const {
+#ifdef EXT_VERSION_BIGTABLE2
+	return EXT_VERSION_BIGTABLE2;
+#else
+	return "";
+#endif
+}
+
+} // namespace duckdb
+
+extern "C" {
+
+DUCKDB_EXTENSION_API void bigtable2_init(duckdb::DatabaseInstance &db) {
+    duckdb::DuckDB db_wrapper(db);
+    db_wrapper.LoadExtension<duckdb::Bigtable2Extension>();
+}
+
+DUCKDB_EXTENSION_API const char *bigtable2_version() {
+	return duckdb::DuckDB::LibraryVersion();
+}
+}
+
+#ifndef DUCKDB_EXTENSION_MAIN
+#error DUCKDB_EXTENSION_MAIN not defined
+#endif
